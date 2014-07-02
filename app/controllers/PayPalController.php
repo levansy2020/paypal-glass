5f8491d8b986e344bb8d4a492151c1416db91441
angelleye <service@angelleye.com>
2014-07-02 08:59:44 -0500

    Adds method for returning PayPal config data

--- app/controllers/PayPalController.php
+++ app/controllers/PayPalController.php
@@ -14,27 +14,36 @@ class PayPalController extends BaseController {
 	|
 	*/
 
-	public function getBalance()
-	{
-        /**
-         *  Run GetBalance API Request
-         */
-
-        $PayPalConfig = array(
+    /**
+     * @return array
+     *
+     * Get an array of config data to pass into PayPal API request methods.
+     */
+    private function getPayPalConfig()
+    {
+        return array(
             'Sandbox' => $_ENV['SANDBOX'],
             'APIUsername' => $_ENV['PAYPAL_API_USERNAME'],
             'APIPassword' => $_ENV['PAYPAL_API_PASSWORD'],
             'APISignature' => $_ENV['PAYPAL_API_SIGNATURE']
         );
+    }
 
-        $PayPal = new PayPal\PayPal($PayPalConfig);
+    /**
+     * @return mixed
+     *
+     * Call PayPal's GetBalance API and pass the result to a balance view.
+     */
+    public function getBalance()
+	{
+        $PayPal = new PayPal\PayPal($this->getPayPalConfig());
         $GBFields = array('returnallcurrencies' => true);
         $PayPalRequestData = array('GBFields'=>$GBFields);
         $PayPalResult = $PayPal->GetBalance($PayPalRequestData);
 
-        $balance = number_format($PayPalResult['BALANCERESULTS'][0]['L_AMT'],2);
+        $Balance = number_format($PayPalResult['BALANCERESULTS'][0]['L_AMT'],2);
 
-        return View::make('paypal/get-balance')->with('balance', $balance);
+        return View::make('paypal/get-balance')->with('balance', $Balance);
 	}
 
 }
