726a572f2c958a34323b73094ea909bf6d60555c
angelleye <service@angelleye.com>
2014-07-06 09:12:30 -0500

    Adds default master for admin theme

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -12,6 +12,8 @@
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/font-awesome/css/font-awesome.css" rel="stylesheet">
 
+    @yield('page_level_css')
+
     <!-- SB Admin CSS - Include with every page -->
     <link href="/css/sb-admin.css" rel="stylesheet">
 
@@ -25,6 +27,8 @@
 <script src="/js/jquery-1.10.2.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 <script src="/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+
+@yield('page_level_js')
 
 <!-- SB Admin Scripts - Include with every page -->
 <script src="/js/sb-admin.js"></script>
