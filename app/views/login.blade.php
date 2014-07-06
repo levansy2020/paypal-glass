03ba8dcfb5a331d361c6f09565f36309f72e6d17
angelleye <service@angelleye.com>
2014-07-06 11:36:11 -0500

    Adds login page outside of master page

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,10 +1,24 @@
-@extends('layouts.default')
+<!DOCTYPE html>
+<html>
 
-@section('page_title')
-<title>Login | PayPal Glass</title>
-@stop
+<head>
 
-@section('content')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Login | PayPal Glass</title>
+
+    <!-- Core CSS - Include with every page -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <!-- SB Admin CSS - Include with every page -->
+    <link href="/css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body>
+
 <div class="container">
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
@@ -35,4 +49,15 @@
         </div>
     </div>
 </div>
-@stop
+
+<!-- Core Scripts - Include with every page -->
+<script src="/js/jquery-1.10.2.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+
+<!-- SB Admin Scripts - Include with every page -->
+<script src="/js/sb-admin.js"></script>
+
+</body>
+
+</html>
