9e183d1b65dc67b8f5d5e771888a9d65dd7f1f83
angelleye <service@angelleye.com>
2014-07-07 08:37:55 -0500

    Refactoring

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -2,93 +2,36 @@
 
 class PageController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
+    protected $PayPal;
+
+    /**
+     * Constructor
+     */
+    public function __construct(PayPal $PayPal)
+    {
+        // PayPal Model Object
+        $this->PayPal = $PayPal;
+    }
+
+    /**
+	 * Display Overview page.
 	 *
 	 * @return Response
 	 */
 	public function index()
 	{
-        $paypal = new PayPal();
-        $current_balance = $paypal->getBalance();
+        // GetBalance
+        $current_balance = $this->PayPal->getBalance();
 
+        // TransactionSearch
         $params = array(
             'number_of_days' => 1
         );
-        $recent_history = $paypal->transactionSearch($params);
+        $recent_history = $this->PayPal->transactionSearch($params);
 
+        // Make View
         $data = array('current_balance' => $current_balance, 'recent_history' => $recent_history);
         return View::make('index')->with('data', $data);
-	}
-
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
 	}
 
 
