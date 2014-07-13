e324b4a93ae19909945009f9aca22dbc7ea02548
angelleye <service@angelleye.com>
2014-07-13 12:54:40 -0500

    Disables details link for Fee Reversals

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -64,7 +64,8 @@
                         <tr class="odd gradeX">
                             <td class="center">{{ $transaction['L_TIMESTAMP'] }}</td>
                             @if (strtoupper($transaction['L_TYPE']) != 'TEMPORARY HOLD'
-                            && strtoupper($transaction['L_TYPE']) != 'MASS PAYMENT SENT')
+                            && strtoupper($transaction['L_TYPE']) != 'MASS PAYMENT SENT'
+                            && strtoupper($transaction['L_TYPE']) != 'FEE REVERSAL')
                             <td class="center"><a href="/transaction/{{ $transaction['L_TRANSACTIONID'] }}">{{ $transaction['L_TRANSACTIONID'] }}</a></td>
                             @else
                             <td class="center">{{ $transaction['L_TRANSACTIONID'] }}</td>
